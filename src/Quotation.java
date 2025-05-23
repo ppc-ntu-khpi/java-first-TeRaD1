4a41aa6d9e1979997ad0ac7afa8c034892589b27
Марущак Дмитро <dimontv162007@gmail.com>
2025-05-23 07:43:39 +0000

    first

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "This above all: to thine own self be true.";
+  String name = "William Shakespeare";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(name);
+  }
+}
